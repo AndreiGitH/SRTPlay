f0e4857034357fd5a188f9b03b2821bc2dc77178
AndreiGitH <andreiconta@gmail.com>
2025-04-28 09:12:22 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -14,7 +14,6 @@ from io import BytesIO
 # —— estilo turbinado ——
 STYLE_SUFFIX = (
     "3d rendered image,"
-    " highly detailed, 4K HDR, masterpiece, "
     "biblical times, "
     "ancient Middle-East setting, 16:9 aspect ratio, no text."
 )
