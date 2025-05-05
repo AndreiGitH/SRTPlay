7b4ff512f13d6cd31f4fe47e8a35d804673f49b2
AndreiGitH <andreiconta@gmail.com>
2025-05-05 16:32:00 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
-    "Pixar style, cinematic lighting, volumetric light, dramatic contrast, "
+    "3D style, cinematic lighting, volumetric light, dramatic contrast, "
     #"Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, "
     "film still, epic composition, highly detailed, masterpiece, "
     "shallow depth-of-field, 35 mm lens, biblical times, "
