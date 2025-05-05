9ba2283f4e86c52485a805bc3c4ac0ec79faaea1
AndreiGitH <andreiconta@gmail.com>
2025-05-05 16:04:05 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
-    "Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, "
+    "Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, hot colors "
     "film still, epic composition, highly detailed, masterpiece, "
     "shallow depth-of-field, 35 mm lens, biblical times, "
     "ancient Middle-East setting, aspect ratio: 16:9, 1024x574, photorealistic, no text overlay."
