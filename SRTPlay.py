cc5676d0b9440d81820592d866a41e3056f1a28d
AndreiGitH <andreiconta@gmail.com>
2025-05-05 16:35:52 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
-    "3D render, cinematic lighting, volumetric light, dramatic contrast, "
+    "pencil handwrite style, "
     #"Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, "
     "film still, epic composition, highly detailed, masterpiece, "
     "shallow depth-of-field, 35 mm lens, biblical times, "
