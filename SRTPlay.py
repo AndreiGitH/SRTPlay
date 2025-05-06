eb07846c2b31d6e2332ecb7d745deaa6c610dcb4
AndreiGitH <andreiconta@gmail.com>
2025-05-05 21:42:58 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -13,8 +13,8 @@ from io import BytesIO
 
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
-    #"pencil handwrite style, "
-    "Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, "
+    "pencil handwrite style, "
+    #"Ultra-realistic, cinematic lighting, volumetric light, dramatic contrast, "
     "film still, epic composition, highly detailed, masterpiece, "
     "shallow depth-of-field, 35 mm lens, biblical times, "
     "ancient Middle-East setting, aspect ratio: 16:9, 1024x574, photorealistic, no text overlay."
