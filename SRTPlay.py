f1238d8839ece341d5e890580c03cd3ed3b8bffa
AndreiGitH <andreiconta@gmail.com>
2025-06-09 21:28:23 -0300

    Update SRTPlay.py

--- SRTPlay.py
+++ SRTPlay.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
-    "icon style, black background, "
+    "vetor style, black background, "
     #"pencil sketch, colored, high detailed, black background, realistic, "
     #"ancient Middle-East setting, wide, "
     "aspect_ratio=16:9, wide."
