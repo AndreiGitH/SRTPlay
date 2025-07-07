a8b38ba6baa82ccc8fca6080c2b2c6ccc642f8eb
AndreiGitH <andreiconta@gmail.com>
2025-07-07 17:24:19 -0300

    Update SRTPlayMini.py

--- SRTPlayMini.py
+++ SRTPlayMini.py
@@ -28,7 +28,7 @@ import time
 STYLE_SUFFIX = (
     #"D.C. Comics, black background, ancient Middle-East setting, no text overlay."
     #"Cinematic and Photorealistic, high detailed, no text overlay."
-    "cartoon drawing style, elderly characters, minimalistic, no text overlay." 
+    "cartoon style, elderly characters, minimalistic, no text overlay." 
 )
 # ─── session_state ─────────────────────────────
 if "imgs" not in st.session_state:
@@ -69,7 +69,7 @@ def gerar_prompt(client_txt, texto: str) -> str:
         #"this biblical scene, with a beautiful ancient Middle Eastern setting. Capture the character emotion. The prompt must end with the quality parameters." # and only one part of image in blue, red ou yellow color.
         #"This biblical scene, set against a beautiful ancient Middle Eastern backdrop. Capture the emotion of the character or simply the beauty of the historical setting. The prompt should end with the quality parameters."
         #"This biblical scene, that bring emotion and a beautiful look, bright and sharpness, few bright spots, set against a beautiful ancient Middle Eastern backdrop. Face emotion when its important. The prompt should end with the quality parameters."
-        "This scene, that bring emotion. The prompt should end with the quality parameters."
+        "This scene. The prompt should end with the quality parameters."
         f"\n\nScene:\n{texto}\n\nQuality parameters:\n{STYLE_SUFFIX}"
     )
     try:
