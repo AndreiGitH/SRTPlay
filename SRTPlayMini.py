ed131dbd76e59339e141b20f05c2bcc4e1b564bf
AndreiGitH <andreiconta@gmail.com>
2025-07-10 00:14:05 -0300

    Update SRTPlayMini.py

--- SRTPlayMini.py
+++ SRTPlayMini.py
@@ -28,7 +28,7 @@ import time
 STYLE_SUFFIX = (
     #"D.C. Comics, black background, ancient Middle-East setting, no text overlay."
     #"Cinematic and Photorealistic, high detailed, no text overlay."
-    "vector style, elderly characters, minimalistic, medium shot, no text overlay." 
+    "vector style, minimalistic, medium shot, no text overlay." 
 )
 # ─── session_state ─────────────────────────────
 if "imgs" not in st.session_state:
