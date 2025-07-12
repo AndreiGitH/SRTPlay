306f36eced5b36633327ad3ebda180e975b243ee
AndreiGitH <andreiconta@gmail.com>
2025-07-12 14:32:45 -0300

    Update SRTPlayMini.py

--- SRTPlayMini.py
+++ SRTPlayMini.py
@@ -27,8 +27,8 @@ import time
 # ─── Configurações ─────────────────────────────
 STYLE_SUFFIX = (
     #"D.C. Comics, black background, ancient Middle-East setting, no text overlay."
-    #"Cinematic and Photorealistic, high detailed, no text overlay."
-    "vector, minimalistic, no text overlay." 
+    "Cinematic and Photorealistic, high detailed, no text overlay."
+    #"vector, minimalistic, no text overlay." 
 )
 # ─── session_state ─────────────────────────────
 if "imgs" not in st.session_state:
@@ -68,8 +68,8 @@ def gerar_prompt(client_txt, texto: str) -> str:
         #"this biblical scene. Always bring a biblical setting, an environment of the time. The prompt must end with the quality parameters. "
         #"this biblical scene, with a beautiful ancient Middle Eastern setting. Capture the character emotion. The prompt must end with the quality parameters." # and only one part of image in blue, red ou yellow color.
         #"This biblical scene, set against a beautiful ancient Middle Eastern backdrop. Capture the emotion of the character or simply the beauty of the historical setting. The prompt should end with the quality parameters."
-        #"This biblical scene, that bring emotion and a beautiful look, bright and sharpness, few bright spots, set against a beautiful ancient Middle Eastern backdrop. Face emotion when its important. The prompt should end with the quality parameters."
-        "This scene with no text. The prompt should end with the quality parameters."
+        "This biblical scene, that bring emotion and a beautiful look, bright and sharpness, set against a beautiful ancient Middle Eastern backdrop. Face emotion when if necessary. The prompt should end with the quality parameters."
+        #"This scene with no text. The prompt should end with the quality parameters."
         f"\n\nScene:\n{texto}\n\nQuality parameters:\n{STYLE_SUFFIX}"
     )
     try:
